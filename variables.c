62248d2f962ec5cb2ea25592bc59cb453d1a7abe
rajesh3d7 <93367633+rajesh3d7@users.noreply.github.com>
2021-11-11 22:33:39 -0800

    Add files via upload

--- variables.c
+++ variables.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 void main()
 {
-	double num1 = 20.0,num2 = 20.2,sub = num1-num2;
-	
-	
-	printf("sub of %f and %f is %f", num1,num2,sub);
+	int a,h,m;
+	printf("enter the minutes :");
+	scanf("%d",&a);
+	printf("%d hour(s) %d minute(s)",h=a/60,m=a%60);
 }
-
